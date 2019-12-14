d79c2435ad693bac40fc827e8f531e672a2f32c3
SMDL-1299 <michelle-1299@hotmail.com>
2019-12-13 22:48:28 -0500

    Base principal

---
+++ README.md
@@ -0,0 +1,4 @@
+# Proyecto para Ingenieria Web
+Este repositorio contiene un ejemplo con archivos base para aplicar el patrón de diseño MVC a nuestra aplicación web para la Secretaría Administrativa de la FISC. 
+Se agrega la carpeta MySQL que contiene los archivos de la base de datos que permiten probar esta aplicación. Recuerde que dependiendo del módulo que le fue asignado, dependerá su diseño de base de datos.
+Recuerde también que sus contenedores deben poseer los volúmenes que hagan referencia a la ruta correcta de su directorio local para su aplicación y su base de datos.
---
+++ app/Views/Layouts/menu.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-dark fixed-top bg-primary flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="?controller=Home&action=index">Secretaría Administrativa</a>
+    <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+            <a class="nav-link" href="#">Salir</a>
+        </li>
+    </ul>
+</nav>
+
+<nav class="col-md-2 d-none d-md-block bg-light sidebar">
+    <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="?controller=Docente&action=index">
+                    Listar Docentes
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?controller=Docente&action=add">
+                    Registrar Docente
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?controller=Administrativo&action=index">
+                    Listar Administrativos
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Reportes
+                </a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
